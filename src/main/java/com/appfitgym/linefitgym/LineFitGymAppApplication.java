8b6139e48c8492f41d23c8ee1a2df880e9c7c14b
Daniel Ganchev <dani.ganchev1989@gmail.com>
2023-11-11 12:42:08 +0200

    starting the project

---
+++ src/main/java/com/appfitgym/linefitgym/LineFitGymAppApplication.java
@@ -0,0 +1,13 @@
+package com.appfitgym.linefitgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineFitGymAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LineFitGymAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appfitgym/linefitgym/LineFitGymAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appfitgym.linefitgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineFitGymAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
